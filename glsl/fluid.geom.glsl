1096ee45d065ebb0bd428a22105a12873dcc4a03
nkusla <nikola.kuslakovic1616@gmail.com>
2024-11-17 13:17:54 +0100

    fix: Fixed a bug in geom shader where ellipse is drawn

--- glsl/fluid.geom.glsl
+++ glsl/fluid.geom.glsl
@@ -3,8 +3,11 @@
 layout(points) in;
 layout(triangle_strip, max_vertices = 34) out;
 
+uniform mat4 view;
+uniform mat4 projection;
+
 const float PI = 3.14159265359;
-const float radius = 0.02;
+const float radius = 0.015;
 
 void main() {
     vec4 center = gl_in[0].gl_Position;
@@ -14,11 +17,11 @@ void main() {
     for (int i = 0; i <= numSegments; ++i) {
         float angle = 2.0 * PI * float(i) / float(numSegments);
         vec4 offset = vec4(cos(angle) * radius, sin(angle) * radius, 0.0, 0.0);
-        gl_Position = center + offset;
+        gl_Position = projection * view * (center + offset);
         EmitVertex();
 
         // Emit the center point again to create a strip
-        gl_Position = center;
+        gl_Position = projection * view * center;
         EmitVertex();
     }
     EndPrimitive();
